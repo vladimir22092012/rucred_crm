addd8a155880b7ca728c30f2285285920217adcf
Vlad R1 <stan4lod@yandex.ru>
2022-04-22 18:38:59 +0400

    test api

--- api/IndexApi.php
+++ api/IndexApi.php
@@ -7,27 +7,7 @@ chdir('..');
 require 'autoload.php';
 
 //получение заголовков из запроса
-$response = curl_exec($ch);
-$headers = get_headers_from_curl_response($response);
-
-function get_headers_from_curl_response($response)
-{
-    $headers = array();
-
-    $header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
-
-    foreach (explode("\r\n", $header_text) as $i => $line)
-        if ($i === 0)
-            $headers['http_code'] = $line;
-        else
-        {
-            list ($key, $value) = explode(': ', $line);
-
-            $headers[$key] = $value;
-        }
-
-    return $headers;
-}
+$headers = getallheaders();
 
 $core = new Core();
 
