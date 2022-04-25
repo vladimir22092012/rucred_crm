ea5cd79b0cf3c6efa38fb72b762726f99f107da4
Vlad R1 <stan4lod@yandex.ru>
2022-04-25 19:07:41 +0300

    fix

--- api/routers/Test.php
+++ api/routers/Test.php
@@ -6,29 +6,28 @@ use Api\apiBaseClass;
 
 class Test extends apiBaseClass {
 
+    public $checkToken  = true;
+
     public function get($vars) {
-      var_dump($vars);
+      var_dump(1111);
     }
 
     public function test() {
         $user = $this->users->get_user(181780);
 
-        $this->get_jwt_token();
-        var_dump($this->checkToken);
+        //$this->get_jwt_token();
+        //var_dump($this->checkToken);
         $res = [
             'method' => 'GET',
             'id' => $user,
             'good' => 'phone',
             'price' => 10000
         ];
-
-        $this->json_response($res);
+var_dump(222);
+        //$this->json_response($res);
         // Выводим ответ клиенту
         /* echo json_encode($res, JSON_PRETTY_PRINT);
         return; */
     }
 
-    public function tme2() {
-      var_dump(12322);
-    }
   }
