3750aacafe99db3d53702d0f89efb72770a53ee4
Zorca Orcinus <vs@zorca.org>
2022-05-06 02:48:09 +0400

    fix: correct path for composer autoload

--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(0);
-ini_set('display_errors', 'On');
 
 session_start();
 
@@ -8,7 +6,7 @@ $start_time = microtime(true);
 
 define('ROOT', rtrim(__DIR__, '\\/'));
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 try 
 {
