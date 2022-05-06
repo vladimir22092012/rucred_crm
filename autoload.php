0d00bfe9c535909cb5ccf0eff4c1b423a92dc59c
Zorca Orcinus <vs@zorca.org>
2022-05-06 04:45:09 +0400

    fix: upload files

--- autoload.php
+++ autoload.php
@@ -2,6 +2,8 @@
 
 function core_autoload($classname)
 {
+    define('ROOT', rtrim(__DIR__, '\\/'));
+
     if (file_exists(dirname(__FILE__).'/core/'.$classname.'.php'))
         require dirname(__FILE__).'/core/'.$classname.'.php';
     if (file_exists(dirname(__FILE__).'/models/'.$classname.'.php'))
--- public/index.php
+++ public/index.php
@@ -4,14 +4,12 @@ session_start();
 
 $start_time = microtime(true);
 
-define('ROOT', rtrim(__DIR__, '\\/'));
-
 require __DIR__ . '/../vendor/autoload.php';
 
-try 
+try
 {
     $view = new IndexController();
-    
+
     if(($res = $view->fetch()) !== false)
     {
         if ($res == 403)
@@ -19,30 +17,30 @@ try
             header("http/1.0 403 Forbidden");
         	$_GET['page_url'] = '403';
         	$_GET['module'] = 'ErrorController';
-        	print $view->fetch();   
+        	print $view->fetch();
         }
         else
         {
         	// Выводим результат
-        	header("Content-type: text/html; charset=UTF-8");	
+        	header("Content-type: text/html; charset=UTF-8");
         	print $res;
-        
+
         }
     }
-    else 
-    { 
+    else
+    {
     	// Иначе страница об ошибке
     	header("http/1.0 404 not found");
-    	
+
     	// Подменим переменную GET, чтобы вывести страницу 404
     	$_GET['page_url'] = '404';
     	$_GET['module'] = 'ErrorController';
-    	print $view->fetch();   
+    	print $view->fetch();
     }
 }
 catch (Exception $e)
 {
-    echo __FILE__.' '.__LINE__.'<br /><pre>';var_dump($e);echo '</pre><hr />'; 
+    echo __FILE__.' '.__LINE__.'<br /><pre>';var_dump($e);echo '</pre><hr />';
 }
 
 $end_time = microtime(true);
