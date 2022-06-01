b65cb478fb4e19f0ebbeeaca7620396d8283873a
Ruslan Kopyl <kopyl.net@gmail.com>
2022-06-01 21:59:30 +0300

    add Addresses model

---
+++ models/Addresses.php
@@ -0,0 +1,114 @@
+<?php
+
+class Addresses extends Core
+{
+	public function get_address($id)
+	{
+		$query = $this->db->placehold("
+            SELECT * 
+            FROM __addresses
+            WHERE id = ?
+        ", (int)$id);
+        $this->db->query($query);
+        $result = $this->db->result();
+	
+        return $result;
+    }
+    
+	public function get_addresses($filter = array())
+	{
+		$id_filter = '';
+        $keyword_filter = '';
+        $limit = 1000;
+		$page = 1;
+        
+        if (!empty($filter['id']))
+            $id_filter = $this->db->placehold("AND id IN (?@)", array_map('intval', (array)$filter['id']));
+        
+		if(isset($filter['keyword']))
+		{
+			$keywords = explode(' ', $filter['keyword']);
+			foreach($keywords as $keyword)
+				$keyword_filter .= $this->db->placehold('AND (name LIKE "%'.$this->db->escape(trim($keyword)).'%" )');
+		}
+        
+		if(isset($filter['limit']))
+			$limit = max(1, intval($filter['limit']));
+
+		if(isset($filter['page']))
+			$page = max(1, intval($filter['page']));
+            
+        $sql_limit = $this->db->placehold(' LIMIT ?, ? ', ($page-1)*$limit, $limit);
+
+        $query = $this->db->placehold("
+            SELECT * 
+            FROM __addresses
+            WHERE 1
+                $id_filter
+				$keyword_filter
+            ORDER BY id DESC 
+            $sql_limit
+        ");
+        $this->db->query($query);
+        $results = $this->db->results();
+        
+        return $results;
+	}
+    
+	public function count_addresses($filter = array())
+	{
+        $id_filter = '';
+        $keyword_filter = '';
+        
+        if (!empty($filter['id']))
+            $id_filter = $this->db->placehold("AND id IN (?@)", array_map('intval', (array)$filter['id']));
+		
+        if(isset($filter['keyword']))
+		{
+			$keywords = explode(' ', $filter['keyword']);
+			foreach($keywords as $keyword)
+				$keyword_filter .= $this->db->placehold('AND (name LIKE "%'.$this->db->escape(trim($keyword)).'%" )');
+		}
+                
+		$query = $this->db->placehold("
+            SELECT COUNT(id) AS count
+            FROM __addresses
+            WHERE 1
+                $id_filter
+                $keyword_filter
+        ");
+        $this->db->query($query);
+        $count = $this->db->result('count');
+	
+        return $count;
+    }
+    
+    public function add_address($address)
+    {
+		$query = $this->db->placehold("
+            INSERT INTO __addresses SET ?%
+        ", (array)$address);
+        $this->db->query($query);
+        $id = $this->db->insert_id();
+        
+        return $id;
+    }
+    
+    public function update_address($id, $address)
+    {
+		$query = $this->db->placehold("
+            UPDATE __addresses SET ?% WHERE id = ?
+        ", (array)$address, (int)$id);
+        $this->db->query($query);
+        
+        return $id;
+    }
+    
+    public function delete_address($id)
+    {
+		$query = $this->db->placehold("
+            DELETE FROM __addresses WHERE id = ?
+        ", (int)$id);
+        $this->db->query($query);
+    }
+}
