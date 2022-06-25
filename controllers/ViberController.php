f2aa49326eaa0233dbe0f7f56e7efb51a40e34fc
Andrew Popov <duircianos@yandex.ru>
2022-06-25 14:24:37 +0400

    добавление либы для вайбер интеграции

--- controllers/ViberController.php
+++ controllers/ViberController.php
@@ -1,16 +1,41 @@
 <?php
 
-use Viber\Client;
+use Viber\Bot;
+use Viber\Api\Sender;
 
 class ViberController extends Controller
 {
     protected $apy_key = '4f668e111aa7defb-b74d69004af9235c-371097ebb1cfa25e';
-    protected $webhookUrl = 'https://rucred-dev.ru/viber';
 
     public function fetch()
     {
-        $client = new Client(['token' => $this->apy_key]);
-        $result = $client->setWebhook($this->webhookUrl);
-        var_dump($result);
+        $botSender = new Sender([
+            'name' => 'Whois bot',
+            'avatar' => 'https://developers.viber.com/img/favicon.ico',
+        ]);
+
+        try {
+            $bot = new Bot(['token' => $this->apiKey]);
+            $bot
+                ->onConversation(function ($event) use ($bot, $botSender) {
+                    // this event fires if user open chat, you can return "welcome message"
+                    // to user, but you can't send more messages!
+                    return (new \Viber\Api\Message\Text())
+                        ->setSender($botSender)
+                        ->setText("Can i help you?");
+                })
+                ->onText('|whois .*|si', function ($event) use ($bot, $botSender) {
+                    // match by template, for example "whois Bogdaan"
+                    $bot->getClient()->sendMessage(
+                        (new \Viber\Api\Message\Text())
+                            ->setSender($botSender)
+                            ->setReceiver($event->getSender()->getId())
+                            ->setText("I do not know )")
+                    );
+                })
+                ->run();
+        } catch (Exception $e) {
+            // todo - log exceptions
+        }
     }
 }
