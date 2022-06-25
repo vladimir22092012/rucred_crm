39c4ffa24d75563bdfd5d7e8452eddbd73e142d8
Andrew Popov <duircianos@yandex.ru>
2022-06-25 14:00:21 +0400

    добавление либы для вайбер интеграции

--- controllers/ViberController.php
+++ controllers/ViberController.php
@@ -1,28 +1,16 @@
 <?php
 
 use Viber\Client;
-exit(123);
 
 class ViberController extends Controller
 {
-    protected $apy_key;
-    protected $webhookUrl;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->apy_key = '4f668e111aa7defb-b74d69004af9235c-371097ebb1cfa25e';
-        $this->webhookUrl = 'https://rucred-dev.ru/viber';
-    }
+    protected $apy_key = '4f668e111aa7defb-b74d69004af9235c-371097ebb1cfa25e';
+    protected $webhookUrl = 'https://rucred-dev.ru/viber';
 
     public function fetch()
     {
-        try {
-            $client = new Client([ 'token' => $this->apy_key ]);
-            $result = $client->setWebhook($this->webhookUrl);
-            echo "Success!\n";
-        } catch (Exception $e) {
-            echo "Error: ". $e->getError() ."\n";
-        }
+        $client = new Client(['token' => $this->apy_key]);
+        $result = $client->setWebhook($this->webhookUrl);
+        var_dump($result);
     }
 }
