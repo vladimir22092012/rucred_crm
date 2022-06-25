fe39820a1db4223e48079fd302f1e094dfe93a5e
Andrew Popov <duircianos@yandex.ru>
2022-06-25 14:31:23 +0400

    добавление либы для вайбер интеграции

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -21,7 +21,7 @@ class IndexController extends Controller
         $module = $this->request->get('module', 'string');
         $module = preg_replace("/[^A-Za-z0-9]+/", "", $module);
 
-        if (!in_array($module, ['LoginController', 'TelegramController', 'ViberController'])&& !$this->manager) {
+        if (!in_array($module, ['LoginController', 'TelegramController', 'ViberController']) && !$this->manager) {
             header('Location: ' . $this->config->root_url . '/login?back=' . $this->request->url());
             exit;
         }
