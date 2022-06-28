dfd63dd89b8f9e7dcc91a23719737561d11b7a85
Andrew Popov <duircianos@yandex.ru>
2022-06-28 14:07:29 +0400

    отсыл доков в яндекс диск

--- models/YaDisk.php
+++ models/YaDisk.php
@@ -1,6 +1,5 @@
 <?php
 
-ini_set('max_execution_time', 300);
 class YaDisk extends Core
 {
     protected $token;
@@ -87,13 +86,143 @@ class YaDisk extends Core
                 if ($upload_scans == 1)
                     $resource->upload($this->config->root_url . '/files/users/' . $order->user_id . '/' . $document->name);
                 else{
+
+                    foreach ($document->params as $param_name => $param_value) {
+                        $this->design->assign($param_name, $param_value);
+                    }
+
+                    $settlement = $this->OrganisationSettlements->get_settlement($document->params->settlement_id);
+
+                    $this->design->assign('settlement', $settlement);
+
+                    $regadress = $this->Addresses->get_address($document->params->regaddress_id);
+                    $this->design->assign('regadress', $regadress);
+
+                    $faktadress = $this->Addresses->get_address($document->params->faktaddress_id);
+                    $this->design->assign('faktadress', $faktadress);
+
+
+                    $company = $this->Companies->get_company($document->params->company_id);
+                    $this->design->assign('company', $company);
+
+                    $code_asp = $this->AspCodes->get_code(['code' => $document->params->sms]);
+                    $this->design->assign('code_asp', $code_asp);
+
+                    $loan_id = $document->params->loan_type;
+                    $loan = $this->Loantypes->get_loantype($loan_id);
+                    $this->design->assign('loan', $loan);
+
+                    $start_date = new DateTime(date('Y-m-d', strtotime($document->params->probably_start_date)));
+                    $end_date = new DateTime(date('Y-m-10', strtotime($document->params->probably_return_date)));
+
+                    $period = date_diff($start_date, $end_date)->days;
+
+                    $this->design->assign('period', $period);
+
+                    $payment_schedule = json_decode($document->params->payment_schedule, true);
+                    $payment_schedule = end($payment_schedule);
+
+                    uksort(
+                        $payment_schedule,
+                        function ($a, $b) {
+
+                            if ($a == $b) {
+                                return 0;
+                            }
+
+                            return (date('Y-m-d', strtotime($a)) < date('Y-m-d', strtotime($b))) ? -1 : 1;
+                        }
+                    );
+
+                    $all_pay_sum_string = explode('.', $payment_schedule['result']['all_sum_pay']);
+
+                    $all_pay_sum_string_part_one = $this->num2str($all_pay_sum_string[0]);
+                    $all_pay_sum_string_part_two = substr($all_pay_sum_string[1], 0, 2);
+
+                    $this->design->assign('all_pay_sum_string_part_two', $all_pay_sum_string_part_two);
+                    $this->design->assign('all_pay_sum_string_part_one', $all_pay_sum_string_part_one);
+
+                    $all_percents_string = explode('.', $payment_schedule['result']['all_loan_percents_pay']);
+                    $all_percents_string_part_one = $this->num2str($all_percents_string[0]);
+
+                    $all_percents_string_part_two = substr($all_percents_string[1], 0, 2);
+                    $this->design->assign('all_percents_string_part_two', $all_percents_string_part_two);
+
+                    $percents_per_day_str = explode('.', $document->params->percent);
+                    $percents_per_day_str_part_one = $this->num2str($percents_per_day_str[0]);
+                    $percents_per_day_str_part_two = $this->num2str($percents_per_day_str[1]);
+
+                    $this->design->assign('percents_per_day_str_part_one', $percents_per_day_str_part_one);
+                    $this->design->assign('percents_per_day_str_part_two', $percents_per_day_str_part_two);
+
+                    $period_str = $this->num2str($period);
+
+                    $this->design->assign('period_str', $period_str);
+
+                    $this->design->assign('all_percents_string_part_one', $all_percents_string_part_one);
+
+                    $first_part_all_sum_pay = explode('.', $payment_schedule['result']['all_sum_pay']);
+                    $first_part_all_sum_pay = $first_part_all_sum_pay[0];
+
+                    $this->design->assign('payment_schedule', $payment_schedule);
+                    $this->design->assign('first_part_all_sum_pay', $first_part_all_sum_pay);
+
+
+                    $percents_per_year = $document->params->psk;
+                    $percents = $percents_per_year;
+
+                    $percents = number_format($percents, 3, ',', ' ');
+
+                    $this->design->assign('percents', $percents);
+                    $percents_str = explode(',', $percents);
+
+                    if (count($percents_str) > 1) {
+                        $second_part_percents = $percents_str[1];
+                        $second_part_percents = $this->num2str($second_part_percents);
+                        $this->design->assign('second_part_percents', $second_part_percents);
+                    }
+
+                    $percents_per_year = $this->num2str($percents_per_year);
+                    $this->design->assign('percents_per_year', $percents_per_year);
+                    $psk_rub = $payment_schedule['result']['all_loan_percents_pay'] + $payment_schedule['result']['all_comission_pay'];
+                    $psk_rub = number_format($psk_rub, 2, ',', ' ');
+
+                    $amount_to_string = explode(',', $psk_rub);
+                    $amount_to_string_1 = str_replace(' ', '', $amount_to_string[0]);
+
+                    $amount_to_string_1 = $this->num2str($amount_to_string_1);
+                    $this->design->assign('amount_to_string_1', $amount_to_string_1);
+
+                    if (isset($amount_to_string[1])) {
+                        $amount_to_string_2 = $this->num2str($amount_to_string[1]);
+                        $this->design->assign('amount_to_string_2', $amount_to_string_2);
+                    }
+
+                    $this->design->assign('psk_rub', $psk_rub);
+
+                    $amount_string = $this->num2str($document->params->amount);
+                    $this->design->assign('amount_string', $amount_string);
+
+                    $passport_serial_full = explode(' ', $document->params->passport_serial);
+                    $passport_serial = $passport_serial_full[0];
+                    $passport_number = $passport_serial_full[1];
+
+                    $this->design->assign('passport_serial', $passport_serial);
+                    $this->design->assign('passport_number', $passport_number);
+
+                    $created_date = new DateTime(date('Y-m-d', strtotime($document->params->date)));
+                    $probably_return_date = new DateTime(date('Y-m-d', strtotime($document->params->probably_return_date)));
+
+                    $period_days = date_diff($created_date, $probably_return_date)->days;
+
+                    $this->design->assign('$period_days', $period_days);
+
                     $tpl = $this->design->fetch('pdf/' . $document->template);
                     $this->pdf->create($tpl, $document->name, $document->template, $download = false, $yandex = $file_name);
                     $resource->upload(ROOT . '/files/users/'. $file_name.'.pdf');
                     unlink(ROOT . '/files/users/'. $file_name.'.pdf');
                 }
             }
-            sleep(5);
         }
     }
 
