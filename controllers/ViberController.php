96b181dd1e420234d245c4327bae004c018ca0b2
Andrew Popov <duircianos@yandex.ru>
2022-07-04 13:33:02 +0400

    дебаг Viber бота

--- controllers/ViberController.php
+++ controllers/ViberController.php
@@ -15,23 +15,13 @@ class ViberController extends Controller
         ]);
 
         $bot = new Bot(['token' => $this->apy_key]);
-        $bot
-            ->onConversation(function ($event) use ($bot, $botSender) {
-                // this event fires if user open chat, you can return "welcome message"
-                // to user, but you can't send more messages!
-                return (new \Viber\Api\Message\Text())
+        $bot->onText('hello', function ($event) use ($bot, $botSender) {
+            $bot->getClient()->sendMessage(
+                (new \Viber\Api\Message\Text())
                     ->setSender($botSender)
-                    ->setText("Can i help you?");
-            })
-            ->onText('hello', function ($event) use ($bot, $botSender) {
-                // match by template, for example "whois Bogdaan"
-                $bot->getClient()->sendMessage(
-                    (new \Viber\Api\Message\Text())
-                        ->setSender($botSender)
-                        ->setReceiver($event->getSender()->getId())
-                        ->setText("hello!")
-                );
-            })
-            ->run();
+                    ->setReceiver($event->getSender()->getId())
+                    ->setText("hello!")
+            );
+        })->run();
     }
 }
