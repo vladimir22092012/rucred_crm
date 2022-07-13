4827e3487882a9dc1a81f2e10cce74e56dd6c9a6
Andrew Popov <duircianos@yandex.ru>
2022-07-13 16:52:28 +0400

    правки по АСП

--- controllers/AspRegistrController.php
+++ controllers/AspRegistrController.php
@@ -13,17 +13,17 @@ class AspRegistrController extends Controller
 
         $codes = $this->AspCodes->get_codes($filter);
 
-        $users = new stdClass();
-
-        foreach($codes as $code){
+        foreach($codes as $key => $code){
             $code->user = $this->users->get_user($code->user_id);
             $code->manager =  $this->managers->get_manager($code->manager_id);
             $code->documents = $this->documents->get_documents(['order_id' => $code->order_id]);
+
+            if(empty($code->documents) || count($code->documents) < 11)
+                unset($codes[$key]);
         }
 
-        $this->design->assign('users', $users);
-
         $this->design->assign('codes', $codes);
+
         return $this->design->fetch('asp_registr.tpl');
     }
 }
