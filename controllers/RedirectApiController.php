37c11a73a8f22db161fde7df23d14087023f775d
Andrew Popov <duircianos@yandex.ru>
2022-07-20 11:54:49 +0400

    дебаг по вайберу

--- controllers/RedirectApiController.php
+++ controllers/RedirectApiController.php
@@ -4,11 +4,6 @@ class RedirectApiController extends Controller
 {
     public function fetch()
     {
-        $registration = $this->request->get('registration');
-
-        if (!empty($registration)){
-            header("Location: viber://pa?chatURI=rucred_bot&registration=$registration");
-            exit;
-        }
+        header("Location: viber://pa?chatURI=rucred_bot");
     }
 }
