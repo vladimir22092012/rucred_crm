57d8dd4e42a7c028e824ab6aa7573e6737193626
Andrew Popov <duircianos@yandex.ru>
2022-07-20 12:53:14 +0400

    дебаг по вайберу

--- controllers/ViberController.php
+++ controllers/ViberController.php
@@ -29,7 +29,7 @@ class ViberController extends Controller
             })
             ->onText('|registration|', function ($event) use ($bot, $botSender) {
 
-                $this->Logs->add(['text' => $event->getMessage()]);
+                $this->Logs->add(['text' => $event->getMessage()->getText()]);
                 die();
                 $chat_id = $event->getSender()->getId();
                 $user = $this->ViberUsers->get_user_by_chat_id($chat_id);
