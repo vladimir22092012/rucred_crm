923c0968ec260085dc01a7f2f648f1b37471d876
Andrew Popov <duircianos@yandex.ru>
2022-07-26 13:41:29 +0400

    поправил платежку для онлайна

--- models/Soap1c.php
+++ models/Soap1c.php
@@ -138,6 +138,8 @@ class Soap1c extends Core
         $item->БИКБанкаКонтрагента = $payment->user_bik;
         $item->ИННПолучателя = '9725055162';
 
+        var_dump($item);
+
         $request = new StdClass();
         $request->TextJSON = json_encode($item);
         $result = $this->send_request('CRM_WebService', 'PaymentOrder', $request);
