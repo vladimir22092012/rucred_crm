ee3768e96f31167c78f8450e4d248e4c236c2c04
Andrew Popov <duircianos@yandex.ru>
2022-07-26 15:48:31 +0400

    кроны для нотификаций

--- cron/SmsNotificationsCron.php
+++ cron/SmsNotificationsCron.php
@@ -20,36 +20,38 @@ class SmsNotificationsCron extends Core
         $is_comlited = 0;
         $crons = $this->NotificationsCron->gets($is_comlited);
 
-        foreach ($crons as $cron) {
-            $ticket = $this->tickets->get_ticket($cron->ticket_id);
+        if(!empty($crons)){
+            foreach ($crons as $cron) {
+                $ticket = $this->tickets->get_ticket($cron->ticket_id);
 
-            if (in_array($ticket->theme_id, [8, 17, 20, 22, 24, 31])) {
-                $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => 'employer']);
-            }
-
-            if (in_array($ticket->theme_id, [11, 13, 18, 23, 25, 26, 27, 28, 29, 30, 32, 33, 34, 35, 38])) {
-                $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => ['underwriter', 'middle']]);
-            }
-
-            if (in_array($ticket->theme_id, [12, 37])) {
-                $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => 'middle']);
-            }
-
-            if (in_array($ticket->theme_id, [27, 28, 29, 30, 32, 33])) {
-                $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => 'admin']);
-            }
-
-            foreach ($managers as $manager) {
-                if($manager->sms_note == 1){
-                    $message = $ticket->text;
-                    $this->sms->send(
-                        $manager->phone,
-                        $message
-                    );
+                if (in_array($ticket->theme_id, [8, 17, 20, 22, 24, 31])) {
+                    $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => 'employer']);
                 }
-            }
 
-            $this->NotificationsCron->update($cron->id, ['is_complited' => 1]);
+                if (in_array($ticket->theme_id, [11, 13, 18, 23, 25, 26, 27, 28, 29, 30, 32, 33, 34, 35, 38])) {
+                    $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => ['underwriter', 'middle']]);
+                }
+
+                if (in_array($ticket->theme_id, [12, 37])) {
+                    $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => 'middle']);
+                }
+
+                if (in_array($ticket->theme_id, [27, 28, 29, 30, 32, 33])) {
+                    $managers = $this->managers->get_managers(['group_id' => $ticket->group_id, 'role' => 'admin']);
+                }
+
+                foreach ($managers as $manager) {
+                    if($manager->sms_note == 1){
+                        $message = $ticket->text;
+                        $this->sms->send(
+                            $manager->phone,
+                            $message
+                        );
+                    }
+                }
+
+                $this->NotificationsCron->update($cron->id, ['is_complited' => 1]);
+            }
         }
     }
 }
