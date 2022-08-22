13edb5b3929f98b5c9f99463beb8ae89ea21489e
Андрей Попов <duircianos@yandex.ru>
2022-08-22 10:14:03 +0400

    правки по крону

--- cron/NotificationsCronRun.php
+++ cron/NotificationsCronRun.php
@@ -7,7 +7,7 @@ use Viber\Client;
 use App\Services\MailService;
 
 error_reporting(-1);
-ini_set('display_errors', 'On');
+ini_set('display_errors', 'Off');
 date_default_timezone_set('Europe/Moscow');
 chdir(dirname(__FILE__) . '/../');
 
