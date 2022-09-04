9cbf4648f436013f911bf69629ef3fc19e8c55b0
Андрей Попов <duircianos@yandex.ru>
2022-09-04 12:17:55 +0400

    логирование всех нотификация для клиентов и манагеров

--- controllers/NeworderController.php
+++ controllers/NeworderController.php
@@ -7,7 +7,7 @@ use Viber\Client;
 use App\Services\MailService;
 
 error_reporting(-1);
-ini_set('display_errors', 'Off');
+ini_set('display_errors', 'On');
 date_default_timezone_set('Europe/Moscow');
 
 class NeworderController extends Controller
