5c4883c3f1521a97e0d1756cdbecf77947794f53
Андрей Попов <duircianos@yandex.ru>
2022-09-13 14:44:05 +0400

    https://trello.com/c/47yjeYIr

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -1051,6 +1051,7 @@ class OrderController extends Controller
 
             //Server settings
             $mail->isSMTP();                                            //Send using SMTP
+            $mail->CharSet = 'UTF-8';
             $mail->Host = 'mail.nic.ru';                          //Set the SMTP server to send through
             $mail->SMTPAuth = true;                                   //Enable SMTP authentication
             $mail->Username = 'noreply@re-aktiv.ru';                  //SMTP username
