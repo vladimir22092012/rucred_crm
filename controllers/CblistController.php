d2b37a6457aa1ea67ca418b2549e07ea0628c7e1
Андрей Попов <duircianos@yandex.ru>
2022-09-28 13:56:51 +0400

    фиксы по графику

--- controllers/CblistController.php
+++ controllers/CblistController.php
@@ -2,44 +2,50 @@
 
 class CblistController extends Controller
 {
-    public $import_file_dir = 'files/import/';
-    public $import_file = 'cblist.xml';
 
     public function fetch()
     {
-        if (!is_writable($this->import_files_dir))
-            $this->design->assign('message_error', 'no_permission');
+        if ($this->request->post('run')) {
+            if (empty($_FILES['import_file'])) {
+                $this->design->assign('error', 'Загрузите файл');
+            } else {
 
-        if (empty($import_file)) {
-            $this->design->assign('error', 'Загрузите файл');
-        } else {
-            $uploaded_name = $this->request->files("import_file", "tmp_name");
+                $xml = simplexml_load_file($_FILES['import_file']['tmp_name']);
+                $xml = $xml->ИнформЧасть;
+                $success = true;
 
-            $xml = simplexml_load_file($uploaded_name);
+                foreach ($xml as $value) {
 
+                    $lastname = $value->Раздел2->Участник->СведФЛИП->ФИОФЛИП->Фам;
+                    $firstname = $value->Раздел2->Участник->СведФЛИП->ФИОФЛИП->Имя;
+                    $patronymic = $value->Раздел2->Участник->СведФЛИП->ФИОФЛИП->Отч;
 
-            $success = true;
+                    $fio = "$lastname $firstname $patronymic";
 
-            foreach ($xml->children() as $value) {
+                    $passport_serial = json_decode($value->Раздел2->Участник->СведФЛИП->СведДокУдЛичн->СерияДок[0]);
+                    $passport_number = json_decode($value->Раздел2->Участник->СведФЛИП->СведДокУдЛичн->НомДок[0]);
+                    $birth           = $value->Раздел2->Участник->СведФЛИП->ДатаРождения;
 
-                $person =
-                    [
-                        'fio' => $value->ФИОФЛИП,
-                        'data_birth' => date('Y-m-d', strtotime($value->ДатаРождения)),
-                        'inn' => $value->ИИНФЛИП,
-                        'number_passport' => $value->НомДок
-                    ];
+                    $person =
+                        [
+                            'fio' => $fio,
+                            'birth' => $birth,
+                            'inn' => json_decode($value->Раздел2->Участник->СведФЛИП->ИИНФЛИП),
+                            'passport_serial' => (string)$passport_serial,
+                            'passport_number' => (string)$passport_number
+                        ];
 
-                $result = $this->cblist->add_person($person);
+                    $result = $this->cblist->add_person($person);
+                }
+
+                if ($result == false) {
+                    $success == false;
+                }
+
+                $this->design->assign('success', $success);
             }
-            var_dump();
-
-            if ($result == false) {
-                $success == false;
-            }
-
-            $this->design->assign('success', $success);
         }
+
         return $this->design->fetch('cblist.tpl');
     }
 }
