c9f1c4deffade50169e4c04954cc9a549c8acbf8
Андрей Попов <duircianos@yandex.ru>
2022-09-30 15:19:14 +0400

    загрузка ядиск доков

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -4173,45 +4173,6 @@ class OrderController extends Controller
         ));
     }
 
-    private function add_first_ticket($order_id, $user_id)
-    {
-        $communication_theme = $this->CommunicationsThemes->get(18);
-        $user = $this->users->get_user($user_id);
-
-        $ticket =
-            [
-                'creator' => $this->manager->id,
-                'creator_company' => 2,
-                'client_lastname' => $user->lastname,
-                'client_firstname' => $user->firstname,
-                'client_patronymic' => $user->patronymic,
-                'head' => $communication_theme->head,
-                'text' => $communication_theme->text,
-                'theme_id' => 18,
-                'company_id' => 2,
-                'group_id' => $user->group_id,
-                'order_id' => $order_id,
-                'status' => 0
-            ];
-
-        $ticket_id = $this->Tickets->add_ticket($ticket);
-        $message =
-            [
-                'message' => $communication_theme->text,
-                'ticket_id' => $ticket_id,
-                'manager_id' => $this->manager->id,
-            ];
-        $this->TicketMessages->add_message($message);
-
-        $cron =
-            [
-                'ticket_id' => $ticket_id,
-                'is_complited' => 0
-            ];
-
-        $this->NotificationsCron->add($cron);
-    }
-
     private function action_next_schedule_date()
     {
         $previous_date = $this->request->post('date');
