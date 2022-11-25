69757b618af53ca3231f1d0af4826aa4fd73d7bd
Андрей Попов <duircianos@yandex.ru>
2022-11-25 11:23:52 +0300

    фиксы по блоку редачивания

--- controllers/OfflineOrderController.php
+++ controllers/OfflineOrderController.php
@@ -4367,7 +4367,7 @@ class OfflineOrderController extends Controller
                 $contracts = $this->contracts->get_contracts(['user_id' => $order->user_id]);
 
                 if (!empty($contracts)) {
-                    $count_contracts = count($contracts);
+                    $count_contracts = count($contracts)+1;
                     $count_contracts = str_pad($count_contracts, 2, '0', STR_PAD_LEFT);
                 } else {
                     $count_contracts = '01';
