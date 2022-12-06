0bcc73ebd08600eb56283280e44423caa95c6201
sheroz <goodmartiandev@gmail.com>
2022-12-06 05:45:21 +0300

    fix: logic for blocking employer accounts when blocking a company

---
+++ app/Models/Credential.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Credential extends Model
+{
+    protected $table = 's_managers_credentials';
+    public $timestamps = false;
+}
