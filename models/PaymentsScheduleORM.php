7ce473321c30d7499a3aa963e968e19bbc5d30ed
Андрей Попов <duircianos@yandex.ru>
2022-12-07 14:06:27 +0400

    присваивание проджект номера

---
+++ models/PaymentsScheduleORM.php
@@ -0,0 +1,8 @@
+<?php
+
+class PaymentsScheduleORM extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 's_payments_schedules';
+    protected $guarded = [];
+    public $timestamps = false;
+}
