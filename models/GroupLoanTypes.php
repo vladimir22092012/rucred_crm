6df74fc2cc84422628434783ed6f7a7518bdf1b8
Андрей Попов <duircianos@yandex.ru>
2023-02-08 15:09:23 +0500

    пдн для оффлайн

--- models/GroupLoanTypes.php
+++ models/GroupLoanTypes.php
@@ -75,6 +75,11 @@ class GroupLoanTypes extends Core
         foreach ($results as $result) {
             $loantype = $this->Loantypes->get_loantype($result->loantype_id, $flag);
 
+            if (in_array($loantype->online_flag, [0])) {
+                continue;
+            }
+
+
             if(empty($loantype))
                 continue;
 
