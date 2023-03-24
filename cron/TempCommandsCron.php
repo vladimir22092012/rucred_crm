59c6ba89920073fba97a2daa46ea503c4822b388
Vladimir Mikhailov <vladimir22092012@mail.ru>
2023-03-24 11:20:25 +0000

    Восстановил номера договоров там где не хватало

--- cron/TempCommandsCron.php
+++ cron/TempCommandsCron.php
@@ -11,11 +11,28 @@ class TempCommandsCron extends Core
     }
 
     private function execute() {
-        foreach ([1,2,3,4,6,7] as $role) {
-            $this->DocksPermissions->add_permission([
-                'docktype_id' => 16,
-                'role_id' => $role,
-            ]);
+        echo 'start cron';
+        //$this->createContracts();
+    }
+
+    private function createContracts() {
+        $orders = OrdersORM::all();
+        echo count($orders).PHP_EOL;
+        foreach ($orders as $order) {
+            $number = ProjectContractNumberORM::query()->where('orderId', '=', $order->id)->first();
+            if (!$number) {
+                $contract = ContractsORM::query()->where('order_id', '=', $order->id)->first();
+                if ($contract) {
+                    ProjectContractNumberORM::create([
+                        'orderId' => $order->id,
+                        'userId' => $order->user_id,
+                        'uid' => $contract->number,
+                        'created' => $order->date,
+                        'updated' => $order->date,
+                    ]);
+                    echo "Order";
+                }
+            }
         }
     }
 }
